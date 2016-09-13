df6bb44d0064b12e52b249d081f3c521f5fb99c9
fakepath01 <fakepath01@gmail.com>
2016-09-13 13:32:32 +0300

    adds changes to main - loop

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hweloo world!")
+	fmt.Printf("%d \t %b \t %#X \n", 42, 42, 42)
+	for i := 60; i < 122; i++ {
+		fmt.Printf("%d \t %b \t %x \t %q \n", i, i, i, i)
+	}
 }
